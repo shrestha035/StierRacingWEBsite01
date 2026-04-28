97d72b420981dfe0f54f0351a4ee495c41033e1a
shrestha035 <shrestha.sunil051@gmail.com>
2026-04-28 12:52:52 +0300

    Import Link from react-router-dom in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import { Reveal, useInView } from "@/hooks/useReveal";
 
 /* =====================================================
