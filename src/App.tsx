4d269851532364eb96f56c098478eb78129a6895
shrestha035 <shrestha.sunil051@gmail.com>
2026-04-28 12:50:32 +0300

    Add SponsorPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import SponsorPage from "./pages/SponsorPage.tsx";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/sponsor" element={<SponsorPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
