1a1fba09f27598ac128e05684367a589d423fd3d
shrestha035 <shrestha.sunil051@gmail.com>
2026-05-05 14:39:52 +0300

    Add Crowdfund route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import SponsorPage from "./pages/SponsorPage.tsx";
+import Crowdfund from "./pages/Crowdfund.tsx";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/sponsor" element={<SponsorPage />} />
+          <Route path="/crowdfund" element={<Crowdfund />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
